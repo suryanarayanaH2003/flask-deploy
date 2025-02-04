332f2ad793ccebd1b618cb03bed0b187f2c9e8d5
naveen-ihub <naveen.s.ihub@snsgroups.com>
2025-02-04 10:56:23 +0530

    done

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({'message': 'Hello, World!'})
+
+# if __name__ == '__main__':
+#     app.run()
