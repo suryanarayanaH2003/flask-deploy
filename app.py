f63e5172a894aa103bbff803aaf30cd73f10574e
naveen-ihub <naveen.s.ihub@snsgroups.com>
2025-02-04 11:03:58 +0530

    fix: add status code to JSON response in hello route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return jsonify({'message': 'Hello, World!'})
+    return jsonify({'message': 'Hello, World!'}, 200)
 
 # if __name__ == '__main__':
 #     app.run()
